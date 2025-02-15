5cb06fbedea05061d6398c2a14c37cb626d7c0e3
arkprocoder <aneesurrehman423@gmail.com>
2025-02-15 19:41:03 +0530

    commit 6

--- project/test.py
+++ project/test.py
@@ -1,10 +1,12 @@
 print("hello this is my new test file")
-def sum(a,b):
+def sum1(a,b):
     return a+b
 
-print(sum(10,20))
+print(sum1(10,20))
 
 print(' iam user 2')
 print("i am user 1")
 print("i am user 3")
 print("i am new commit for reset")
+
+print("dont change me")
